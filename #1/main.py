acf2f2ea2f514bf34949927e4d3b7d150c521bac
CaelanYoung2000 <100617735+CaelanYoung2000@users.noreply.github.com>
2022-02-28 13:12:48 -0600

    Add files via upload

---
+++ #1/main.py
@@ -0,0 +1,44 @@
+#Homework 01 - Caelan Young 08/30/2020
+from turtle import *
+# or
+import turtle
+
+print("\nThe product of the first 10 integers is:", 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10)
+
+print("\n /\_/\\")
+print("( ' ' ) / Coding In\\")
+print(" ( - ) <| Python is |")
+print("| | | | \ So Fun!/")
+print("(__|__)")
+
+print("\n Sales Tax Rates ")
+print("___________________")
+print("California:      7.25%")
+print("Florida:         6.00%")
+print("New Mexico:      5.125%")
+print("Oklahoma:        4.50%")
+print("Texas:           6.25%")
+
+turtle.shape("turtle")
+turtle.speed(3)
+turtle.forward(200)
+
+turtle.right(90)
+turtle.forward(200)
+
+turtle.right(90)
+turtle.forward(200)
+
+turtle.right(90)
+turtle.forward(200)
+
+r = 60
+turtle.circle(r)
+
+turtle.left(90)
+
+turtle.circle(r)
+
+turtle.left(90)
+
+turtle.circle(r)
